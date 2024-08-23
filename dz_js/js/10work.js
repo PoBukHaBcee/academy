c376df6b7bb2ebd93e33dd2a1856d398d39823ba
pobukhabcee <mautfat@mail.ru>
2024-08-23 12:50:45 +0300

    d

--- dz_js/js/10work.js
+++ dz_js/js/10work.js
@@ -1,17 +1,16 @@
 class Animal {
 
     constructor(name) {
-      this.name = name;
+      this.name = name
     }
   
   }
-  
   class Rabbit extends Animal {
     constructor(name) {
         super(name);
-      this.created = Date.now();
+      this.created = Date.now()
     }
   }
   
-  let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
-  alert(rabbit.name);
+  let rabbit = new Rabbit("Белый кролик")
+  console.log(rabbit.name)
